c37f61d21fc06e99f3f5f82decee9d958e6036b8
SHASHI YADAV <shashi.yadav.ug23@nsut.ac.in>
2025-09-22 23:31:18 +0530

    Fix typo in image import path for Login.jsx

--- code/frontend/src/pages/Login.jsx
+++ code/frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext'; 
 import { Link } from 'react-router-dom';
-import img from "/assests/loginSignupbg.jpg"; 
+import img from "/assests/loginSignupBg.jpg"; 
 export default function Login() {
     const { login } = useAuth(); 
     const [formData, setFormData] = useState({
